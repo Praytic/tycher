717825bef94305fdf44feec448e51dfc5e51ed74
Kvel2D <kvelyognaut@gmail.com>
2017-07-07 15:35:01 +0300

    make game loop sync with canvas frame refresh
    make backend send game state when receiving inputs, instead of sending
    state at interval
    add mouse inputs

--- src/main/kotlin/ChatWebSocketHandler.kt
+++ src/main/kotlin/ChatWebSocketHandler.kt
@@ -3,93 +3,26 @@ import org.eclipse.jetty.websocket.api.*
 import org.eclipse.jetty.websocket.api.annotations.*
 import org.json.JSONArray
 import org.json.JSONObject
+import java.awt.event.KeyEvent
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.concurrent.scheduleAtFixedRate
 
-class Vector2D {
-    constructor(x: Int, y: Int) {
-        this.x = x;
-        this.y = y;
-    }
-
-    var x = 0;
-    var y = 0;
-}
+data class Vector2D(var x: Int, var y: Int)
 
 @WebSocket
 class ChatWebSocketHandler {
 
     var nextUserNumber = 1
-    val userUsernameMap: MutableMap<Session, String> = ConcurrentHashMap();
-    
-    val userInputs: MutableMap<Session, JSONObject> = ConcurrentHashMap();
-    var userPositions: MutableMap<Session, Vector2D> = ConcurrentHashMap();
-
-    init {
-        val timer = Timer("schedule", true);
-        timer.scheduleAtFixedRate(0, 1000 / 60) {
-            update();
-        }
-    }
-
-    fun update() {
-        // Update positions
-        for ((key, value) in userInputs) {
-            var keys: JSONArray = value.getJSONArray("keys");
-
-            fun contains(array: JSONArray, value: Int): Boolean {
-                for (e in array) {
-                    if (e == value) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            var LEFT = contains(keys, Key.A);
-            var RIGHT = contains(keys, Key.D);
-            var UP = contains(keys, Key.W);
-            var DOWN = contains(keys, Key.S);
-            var x = userPositions[key]!!.x;
-            var y = userPositions[key]!!.y;
-            if (LEFT && !RIGHT) {
-                x--;
-            } else if (!LEFT && RIGHT) {
-                x++;
-            } else if (UP && !DOWN) {
-                y--;
-            } else if (!UP && DOWN) {
-                y++;
-            }
-            userPositions[key]!!.x = x;
-            userPositions[key]!!.y = y;
-        }
-
-        var positions = mutableListOf<Int>();
-        for (entry in userPositions) {
-            positions.add(entry.value.x);
-            positions.add(entry.value.y);
-        }
-
-        // Send data to users
-        userUsernameMap.keys.stream().filter({ it.isOpen() }).forEach { session ->
-            try {
-                session.remote.sendString(JSONObject().put("positions", JSONArray(positions)).toString());
-            } catch (e: Exception) {
-                session.close();
-//                e.printStackTrace();
-            }
-        }
-    }
+    val userUsernameMap: MutableMap<Session, String> = ConcurrentHashMap()
+    var userPositions: MutableMap<Session, Vector2D> = ConcurrentHashMap()
 
     @OnWebSocketConnect
     fun onConnect(user: Session) {
         val username = "User" + nextUserNumber++
         userUsernameMap.put(user, username)
         broadcastMessage(sender = "Server", message = "$username joined the chat")
-        userPositions.put(user, Vector2D(50, 50));
+        userPositions.put(user, Vector2D(50, 50))
     }
 
     @OnWebSocketClose
@@ -101,20 +34,47 @@ class ChatWebSocketHandler {
 
     @OnWebSocketMessage
     fun onMessage(user: Session, message: String) {
-        // Receive inputs from user
         try {
-            userInputs[user] = JSONObject(message);
+            // Update position based on input
+            var inputs = JSONObject(message)
+            val keys = inputs.getJSONArray("keys")
+            val LEFT = keys[KeyEvent.VK_A] == 1
+            val RIGHT = keys[KeyEvent.VK_D] == 1
+            val UP = keys[KeyEvent.VK_W] == 1
+            val DOWN = keys[KeyEvent.VK_S] == 1
+            var x = userPositions[user]!!.x
+            var y = userPositions[user]!!.y
+
+            if (LEFT && !RIGHT) {
+                x--
+            } else if (!LEFT && RIGHT) {
+                x++
+            } else if (UP && !DOWN) {
+                y--
+            } else if (!UP && DOWN) {
+                y++
+            }
+            userPositions[user]!!.x = x
+            userPositions[user]!!.y = y
         } catch (e: Exception) {
-            e.printStackTrace();
+            e.printStackTrace()
         }
 
-//        userUsernameMap.keys.stream().filter({ it.isOpen() && it != user }).forEach { session ->
-//            try {
-//                session.remote.sendString(JSONObject().put("point", JSONObject(message)).toString());
-//            } catch (e: Exception) {
+        val positions = mutableListOf<Int>()
+        for (entry in userPositions) {
+            positions.add(entry.value.x)
+            positions.add(entry.value.y)
+        }
+
+        // Send data to user
+        if (user.isOpen) {
+            try {
+                user.remote.sendString(JSONObject().put("positions", JSONArray(positions)).toString())
+            } catch (e: Exception) {
+                user.close()
 //                e.printStackTrace();
-//            }
-//        }
+            }
+        }
     }
 
 
@@ -141,108 +101,3 @@ class ChatWebSocketHandler {
     }
 
 }
-
-
-object Key {
-    val NUMBER_0: Int = 48;
-    val NUMBER_1: Int = 49;
-    val NUMBER_2: Int = 50;
-    val NUMBER_3: Int = 51;
-    val NUMBER_4: Int = 52;
-    val NUMBER_5: Int = 53;
-    val NUMBER_6: Int = 54;
-    val NUMBER_7: Int = 55;
-    val NUMBER_8: Int = 56;
-    val NUMBER_9: Int = 57;
-    val A: Int = 65;
-    val B: Int = 66;
-    val C: Int = 67;
-    val D: Int = 68;
-    val E: Int = 69;
-    val F: Int = 70;
-    val G: Int = 71;
-    val H: Int = 72;
-    val I: Int = 73;
-    val J: Int = 74;
-    val K: Int = 75;
-    val L: Int = 76;
-    val M: Int = 77;
-    val N: Int = 78;
-    val O: Int = 79;
-    val P: Int = 80;
-    val Q: Int = 81;
-    val R: Int = 82;
-    val S: Int = 83;
-    val T: Int = 84;
-    val U: Int = 85;
-    val V: Int = 86;
-    val W: Int = 87;
-    val X: Int = 88;
-    val Y: Int = 89;
-    val Z: Int = 90;
-    val NUMPAD_0: Int = 96;
-    val NUMPAD_1: Int = 97;
-    val NUMPAD_2: Int = 98;
-    val NUMPAD_3: Int = 99;
-    val NUMPAD_4: Int = 100;
-    val NUMPAD_5: Int = 101;
-    val NUMPAD_6: Int = 102;
-    val NUMPAD_7: Int = 103;
-    val NUMPAD_8: Int = 104;
-    val NUMPAD_9: Int = 105;
-    val NUMPAD_MULTIPLY: Int = 106;
-    val NUMPAD_ADD: Int = 107;
-    val NUMPAD_ENTER: Int = 108;
-    val NUMPAD_SUBTRACT: Int = 109;
-    val NUMPAD_DECIMAL: Int = 110;
-    val NUMPAD_DIVIDE: Int = 111;
-    val F1: Int = 112;
-    val F2: Int = 113;
-    val F3: Int = 114;
-    val F4: Int = 115;
-    val F5: Int = 116;
-    val F6: Int = 117;
-    val F7: Int = 118;
-    val F8: Int = 119;
-    val F9: Int = 120;
-    val F10: Int = 121;
-    val F11: Int = 122;
-    val F12: Int = 123;
-    val F13: Int = 124;
-    val F14: Int = 125;
-    val F15: Int = 126;
-    val BACKSPACE: Int = 8;
-    val TAB: Int = 9;
-    val ALTERNATE: Int = 18;
-    val ENTER: Int = 13;
-    val COMMAND: Int = 15;
-    val SHIFT: Int = 16;
-    val CONTROL: Int = 17;
-    val BREAK: Int = 19;
-    val CAPS_LOCK: Int = 20;
-    val NUMPAD: Int = 21;
-    val ESCAPE: Int = 27;
-    val SPACE: Int = 32;
-    val PAGE_UP: Int = 33;
-    val PAGE_DOWN: Int = 34;
-    val END: Int = 35;
-    val HOME: Int = 36;
-    val LEFT: Int = 37;
-    val RIGHT: Int = 39;
-    val UP: Int = 38;
-    val DOWN: Int = 40;
-    val INSERT: Int = 45;
-    val DELETE: Int = 46;
-    val NUMLOCK: Int = 144;
-    val SEMICOLON: Int = 186;
-    val EQUAL: Int = 187;
-    val COMMA: Int = 188;
-    val MINUS: Int = 189;
-    val PERIOD: Int = 190;
-    val SLASH: Int = 191;
-    val BACKQUOTE: Int = 192;
-    val LEFTBRACKET: Int = 219;
-    val BACKSLASH: Int = 220;
-    val RIGHTBRACKET: Int = 221;
-    val QUOTE: Int = 222;
-};
--- src/main/resources/public/websocketDemo.js
+++ src/main/resources/public/websocketDemo.js
@@ -1,7 +1,6 @@
-//Establish the WebSocket connection and set up event handlers
+// Establish the WebSocket connection and set up event handlers
 var webSocket = new WebSocket("ws://" + location.hostname + ":" + "4567" + "/chat");
 webSocket.onopen = function () {
-  setInterval(update, 1000 / 60);
 };
 webSocket.onmessage = function (msg) {
   handleMessage(msg);
@@ -10,20 +9,21 @@ webSocket.onclose = function () {
   alert("WebSocket connection closed")
 };
 
-window.addEventListener('keydown', function(event) {
-  inputs.push(event.which);
-}, false);
-
-
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-canvas.addEventListener('click', function(e) {
-  // var pos = getMousePos(canvas, e);
-  // webSocket.send(JSON.stringify(pos));
-  // draw(pos);
+
+
+window.addEventListener('keydown', function(event) {
+  keys[event.which] = 1;
 }, false);
 var mouseX = 0;
 var mouseY = 0;
+canvas.addEventListener('click', function(e) {
+  var pos = getMousePos(canvas, e);
+  click_keys.push(event.which);
+  click_positions.push(pos.x);
+  click_positions.push(pos.y);
+}, false);
 canvas.addEventListener('mousemove', function(e) {
   var pos = getMousePos(canvas, e);
   mouseX = pos.x;
@@ -32,26 +32,44 @@ canvas.addEventListener('mousemove', function(e) {
 
 
 var frame = 0;
-var inputs = [0];
 var positions = [];
+var keys = Array(223).fill(-1);
+var click_keys = Array(4).fill(-1);
+var click_positions = [];
+var lastTime = (new Date()).getTime();
+var delta = 0;
+// https://coderwall.com/p/iygcpa/gameloop-the-correct-way
 function update() {
-  frame++;
-  
+  window.requestAnimationFrame(update);
+  currentTime = (new Date()).getTime();
+  delta = (currentTime - lastTime) / 1000;
+
   // Send inputs to server
-  if (inputs.length != 0) {
-    webSocket.send(JSON.stringify({
-      keys: inputs, 
-      mouseX: mouseX, 
-      mouseY: mouseY,
-    }));
-    inputs = [0];
-  }
+  webSocket.send(JSON.stringify({
+    keys: keys, 
+    click_keys: click_keys,
+    click_positions: click_positions,
+    mouseX: mouseX, 
+    mouseY: mouseY,
+  }));
+  keys.fill(-1);
+  click_keys.fill(-1);
+  click_positions = [];
 
   // Draw users
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.font = "30px Arial";
   for (var i = 0; i < positions.length; i++) {
     context.fillText("a", positions[i * 2], positions[i * 2 + 1]);
   }
+
+  frame++;
+  context.fillText(frame.toString(), 5, 25);
+
+  lastTime = currentTime;
 }
+// Start loop
+update();
 
 
 function handleMessage(msg) {
@@ -76,7 +94,6 @@ function updateChat(msg) {
     insert("userlist", "<li>" + user + "</li>");
   });
 }
-
 
 //Helper function for inserting HTML as the first child of an element
 function insert(targetId, message) {
@@ -105,107 +122,3 @@ function getMousePos(canvas, evt) {
     y: evt.clientY - rect.top
   };
 }
-
-var Key = {
-  NUMBER_0: 48,
-  NUMBER_1: 49,
-  NUMBER_2: 50,
-  NUMBER_3: 51,
-  NUMBER_4: 52,
-  NUMBER_5: 53,
-  NUMBER_6: 54,
-  NUMBER_7: 55,
-  NUMBER_8: 56, 
-  NUMBER_9: 57,
-  A: 65,
-  B: 66,
-  C: 67,
-  D: 68,
-  E: 69,
-  F: 70,
-  G: 71,
-  H: 72,
-  I: 73,
-  J: 74,
-  K: 75,
-  L: 76,
-  M: 77,
-  N: 78,
-  O: 79,
-  P: 80,
-  Q: 81,
-  R: 82,
-  S: 83,
-  T: 84,
-  U: 85,
-  V: 86,
-  W: 87,
-  X: 88,
-  Y: 89,
-  Z: 90,
-  NUMPAD_0: 96,
-  NUMPAD_1: 97,
-  NUMPAD_2: 98,
-  NUMPAD_3: 99,
-  NUMPAD_4: 100,
-  NUMPAD_5: 101,
-  NUMPAD_6: 102,
-  NUMPAD_7: 103,
-  NUMPAD_8: 104,
-  NUMPAD_9: 105,
-  NUMPAD_MULTIPLY: 106,
-  NUMPAD_ADD: 107,
-  NUMPAD_ENTER: 108,
-  NUMPAD_SUBTRACT: 109,
-  NUMPAD_DECIMAL: 110,
-  NUMPAD_DIVIDE: 111,
-  F1: 112,
-  F2: 113,
-  F3: 114,
-  F4: 115,
-  F5: 116,
-  F6: 117,
-  F7: 118,
-  F8: 119,
-  F9: 120,
-  F10: 121, //  F10 is used by browser.
-  F11: 122,
-  F12: 123,
-  F13: 124,
-  F14: 125,
-  F15: 126,
-  BACKSPACE: 8,
-  TAB: 9,
-  ALTERNATE: 18,
-  ENTER: 13,
-  COMMAND: 15,
-  SHIFT: 16,
-  CONTROL: 17,
-  BREAK: 19,
-  CAPS_LOCK: 20,
-  NUMPAD: 21,
-  ESCAPE: 27,
-  SPACE: 32,
-  PAGE_UP: 33,
-  PAGE_DOWN: 34,
-  END: 35,
-  HOME: 36,
-  LEFT: 37,
-  RIGHT: 39,
-  UP: 38,
-  DOWN: 40,
-  INSERT: 45,
-  DELETE: 46,
-  NUMLOCK: 144,
-  SEMICOLON: 186,
-  EQUAL: 187,
-  COMMA: 188,
-  MINUS: 189,
-  PERIOD: 190,
-  SLASH: 191,
-  BACKQUOTE: 192,
-  LEFTBRACKET: 219,
-  BACKSLASH: 220,
-  RIGHTBRACKET: 221,
-  QUOTE: 222,
-};
