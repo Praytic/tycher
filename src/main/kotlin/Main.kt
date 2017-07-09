6028874333caaf6930313e25523ac346130b17a2
vchernogorov <vchernogorov@griddynamics.com>
2017-07-09 13:36:11 +0400

    Create BoardHandler and GameHandler

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,38 @@
-import spark.Session
-import spark.Spark
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import org.eclipse.jetty.websocket.api.Session
+import spark.Spark.*
 import java.awt.Point
+import java.util.concurrent.ConcurrentHashMap
+
+val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+val users: MutableMap<Session, User?> = ConcurrentHashMap()
+val tychs: MutableMap<String, Tych> = ConcurrentHashMap()
+
+fun main(args: Array<String>) {
+    staticFiles.location("/public")
+    webSocket("/game", GameHandler::javaClass)
+    webSocket("/board", BoardHandler::javaClass)
+    init()
+}
+
 
 data class User(val name: String,
                 var score: Int = 0,
-                var reloadSpeed: Double = 1.0)
-data class Tych(val tycherName: String,
+                var reloadSpeed: Double = 1.0,
+                var tychIsReady: Boolean = true)
+
+data class Tych(val tycher: User,
                 val position: Point,
                 val initialRadius: Double,
                 val shrinkSpeed: Double = 1.0,
                 val spawnTime: Long,
-                val dummy: Boolean = false)
-data class ScoreBoard(val scores: Map<String, Int>,
-                      val limit: Int = 10)
+                val dummy: Boolean = false) {
+
+    fun isConsumedBy(tych: Tych): Boolean {
+        TODO()
+    }
+}
+
 data class Food(val position: Point,
                 val initialRadius: Double)
-
-fun main(args: Array<String>) {
-    Spark.staticFiles.location("/public") //index.html is served at localhost:4567 (default port)
-    Spark.staticFiles.expireTime(600)
-    Spark.webSocket("/chat", ChatWebSocketHandler::class.java)
-    Spark.init()
-}
