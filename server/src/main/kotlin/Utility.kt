0308073fc3fd81d8c64d9315b4be107fb47e14cd
vchernogorov <vchernogorov@griddynamics.com>
2018-05-10 03:00:47 +0400

    Fix tych remove scheduling

--- server/src/main/kotlin/Utility.kt
+++ server/src/main/kotlin/Utility.kt
@@ -22,16 +22,16 @@ fun <K> MutableMap<K, Tych>.putTemp(key: K, value: Tych, now: Date = Date()) {
   put(key, value)
   val lifeDuration = value.getLifeDurationMillis(now)
 
-  Timer().scheduleAtFixedRate(object : TimerTask() {
+  Timer().schedule(object : TimerTask() {
     override fun run() {
       if (remove(key) != null) {
         val nowSeconds = TimeUnit.MILLISECONDS.toSeconds(Date().time)
-        log.info { "$value was removed after ${lifeDuration / 1000} seconds. " +
+        log.info { "$value was removed after ${lifeDuration / 1000.0} seconds. " +
             "Timestamp in seconds: $nowSeconds." }
       }
     }
-  }, 0L, lifeDuration)
+  }, lifeDuration)
   val nowSeconds = TimeUnit.MILLISECONDS.toSeconds(now.time)
-  log.info { "$value will be removed after ${lifeDuration / 1000} seconds. " +
+  log.info { "$value will be removed after ${lifeDuration / 1000.0} seconds. " +
       "Timestamp in seconds: $nowSeconds." }
 }
